72cf363930ea0004121408ac8e422cd9da2e56c6
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-26 19:31:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def word_substituter(strings)
+  
+  
+  def dictionary
+    dictionary = {
+    "hello" => "hi"
+    "to" => "2"
+    "two" => "2"
+    "too" => "2"
+    "be" => "b"
+    "you" => "u"
+    "at" => "@"
+    "and" => "&"
+    }
+    def word_substituter(tweet)
+      tweet.split.collect do |word|
+        if dictionary.keys.include?(word.downcase)
+          word = dictionary[word.downcase]
+          
